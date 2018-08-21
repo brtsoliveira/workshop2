a9c76a92ab4c2394f846e702274784ebf0e97650
brtsoliveira <35262604+brtsoliveira@users.noreply.github.com>
2018-08-21 11:14:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-echo "<h1>Openshift Workshop v2.3</h1> ";
+echo "<h1>Openshift Workshop v2.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
-$conn = new mysqli("mysql", "redhat@123", "redhat", "redhat@123");
+$conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
